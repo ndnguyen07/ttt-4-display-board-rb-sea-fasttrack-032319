d6dfca0857750ec8c4e95fa3205231f0ac85c9a4
pragmatic-invocation-7908 <ndang.nguyen@hotmail.com>
2019-03-23 22:03:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
- puts "   |   |   "
+ puts " #{board[0]}  | #{board[1]  |   "
  11.times {print "-"}
  puts ""
  puts "   |   |   "
@@ -10,4 +10,4 @@ def display_board (board)
  puts "   |   |   "
 end
 
-puts display_board 
+puts display_board ([" "," "," "," "," "," "," "," "," "])
